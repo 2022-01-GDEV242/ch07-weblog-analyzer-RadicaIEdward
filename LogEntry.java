6cc513b6426009fbbcc6b28a86e0a02983e0fc45
RadicaIEdward <edward.o.galindez@gmail.com>
2022-03-14 13:11:58 -0400

    Created getMonth() method in LogEntry class

--- LogEntry.java
+++ LogEntry.java
@@ -64,6 +64,15 @@ public class LogEntry implements Comparable<LogEntry>
     }
     
     /**
+     * Return the month.
+     * @return The month field from the log line.
+     */
+    public int getMonth()
+    {
+        return dataValues[MONTH];
+    }
+    
+    /**
      * Return the day.
      * @return The day field from the log line.
      */
